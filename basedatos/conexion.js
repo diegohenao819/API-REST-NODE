ed377546bb1a1a5c13b39f8696a84376cd7d9c78
diegohenao819 <diegohenao819@gmail.com>
2022-12-23 00:27:03 -0500

    cambios finales

--- basedatos/conexion.js
+++ basedatos/conexion.js
@@ -3,10 +3,7 @@ require("dotenv").config();
 
 const conexion = async () => {
   try {
-    await mongoose.connect(
-      // `mongodb+srv://diegohenao819:${process.env.PASSWORD}@cluster0.lgv6vcv.mongodb.net/?retryWrites=true&w=majority`
-      "mongodb://mongo:ZPd9gpjpTH6XM072n4rd@containers-us-west-147.railway.app:7393"
-    );
+    await mongoose.connect(process.env.DB_URI);
 
     console.log("Conectado a mi base de datos");
   } catch (error) {
