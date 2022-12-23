b6ccd86c8a3fab0bf7e65729e88e1f32782512b8
diegohenao819 <diegohenao819@gmail.com>
2022-12-23 00:20:28 -0500

    cambio de db

--- basedatos/conexion.js
+++ basedatos/conexion.js
@@ -5,7 +5,7 @@ const conexion = async () => {
   try {
     await mongoose.connect(
       // `mongodb+srv://diegohenao819:${process.env.PASSWORD}@cluster0.lgv6vcv.mongodb.net/?retryWrites=true&w=majority`
-      "mongodb://mongo:j7uuAUTbW2Ja4niG8jXm@containers-us-west-18.railway.app:7263"
+      "mongodb://mongo:ZPd9gpjpTH6XM072n4rd@containers-us-west-147.railway.app:7393"
     );
 
     console.log("Conectado a mi base de datos");
